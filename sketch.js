b627f6baaa1e7d02a33947600641c73b074913f8
shield10724s <71579406+shield10724s@users.noreply.github.com>
2020-11-17 13:55:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ground;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 400);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  
+  drawSprites();
+ 
+  ground.display();
+}
